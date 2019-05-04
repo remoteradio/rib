590a77a55f6b90c58d7e6d896c3d979943217d98
Garth Hitchens <ghitchens@me.com>
2019-05-04 15:39:30 -0700

    refactored API, cleanup, docs

--- lib/daqc.ex
+++ lib/daqc.ex
@@ -1,252 +1,305 @@
 defmodule DACQ do
 
-    @moduledoc """
-    Support for pi-plates (DACQ board only, for now)
+  @moduledoc """
+  Support for pi-plates (DACQ board only, for now)
 
-    ## Implementation Notes
+  ## Implementation Notes
 
-    1.  Started as a more or less direct translation of ppDAQC.py, which is the Python
-        "reference" implementation.   As I went along, I decided to move to more
-        to elixir-y idioms, so the API is now quite a bit different.
+  ### 1. API
 
-    2.  Initially the low level i/o copied the python ppCMD() logic directly, but the
-        elixir verions of getADCall() and getID() functions were unstable or returned wrong results.
-        Upon investigation, the python ones did also.   The python getID() function
-        doesn't seem to work reliably on a PI 3B+ nor does the getADCall().  The
-        elixir versions of these functions now work properly, by inserting a small
-        delay (1ms) between each byte read (not just at the beginning of reading a
-        response).   This has resulted in proper and reliable operation.
+  This Started as a more or less direct translation of ppDACQ.py, which is the
+  Python "reference" implementation. As I  went along, I decided to move to more
+  to elixir-y idioms, so the API is now quite a bit different.
 
-        Note that the python implementation seems to specify a 100us sleep before
-        reading a value, however, in python a sleep() relinquishes the cpu to the
-        OS anyway, which results in a minimum OS scheduler latency of about
-        3-10ms in linux, whereas his only releases the CPU to the erlang VM,
-        which will hold fairly tightly to 1ms, so the Elixir implementation should
-        usually result in lower overall latency than the python version.   In addition,
-        the requirement for these delays will block the entire python interpreter,
-        due to the GIL.   In Elixir, this only blocks the process actually
-        interacting with the pi-plate, allowing building reliable and performant
-        systems.
+  I decided to store "global" references in ets, and eliminate passing a ref
+  to each function, making all functions effectively global. I realize this will
+  be controversial for savvy elixir/erlang readers, however this makes the
+  library more approachable to beginners, and in fact, there can only ever be one
+  instance of pi-plates running in any running vm anyway, due to the way that
+  pi-plates manages SPI I/O.
 
-    3.  I decided to store "global" references in ets, and eliminate passing a ref
-        to each function, making all functions effectively global.   I realize this will
-        be controversial for savvy elixir/erlang readers, however this makes the library
-        more approachable to beginners, and in fact, there can only ever be one
-        instance of pi-plates running in any running vm anyway, due to the way that
-        pi-plates manages SPI I/O.
-    """
+  ### 2. Stability and Delay for SPI
 
-    # allow ease of referencing Circuits.GPIO.whatever(), as just GPIO.whatever()
+  Initially the low level i/o copied the python ppCMD() logic directly, but
+  the elixir verions of getADCall() and getID() functions were unstable or
+  returned wrong results. Upon investigation, the python ones did also!!
 
-    alias Circuits.SPI
-    alias Circuits.GPIO
+  The python getID() function doesn't seem to work reliably on a PI 3B+ nor
+  does the getADCall(). The elixir versions of these functions now work
+  properly, by inserting a small delay (1ms) between each byte read (not
+  just at the beginning of reading a response). This has resulted in proper
+  and reliable operation.
 
-    @dacq_read_delay    1           # 1ms delay before each DACQ uController read
+  Note that the python implementation seems to specify a 100us sleep before
+  reading a value, however, in python a sleep() relinquishes the cpu to the
+  OS anyway, which results in a minimum OS scheduler latency of about
+  3-10ms in linux, whereas his only releases the CPU to the erlang VM,
+  which will hold fairly tightly to 1ms, so the Elixir implementation
+  should usually result in lower overall latency than the python version.
+  In addition, the requirement for these delays will block the entire
+  python interpreter, due to the GIL. In Elixir, this only blocks the
+  process actually interacting with the pi-plate, allowing building
+  reliable and performant systems.
+"""
 
-    @doc """
-    Initalizes the board
+  # allow ease of referencing Circuits.GPIO.whatever(), as just GPIO.whatever()
 
-    Sets up the "global I/O context" in ETS so it can be retreived by get_io()
-    Initializes basic features of the board.
-    """
-    @spec init() :: :ok | {:error, :already_initialized}
+  alias Circuits.SPI
+  alias Circuits.GPIO
 
-    @ppFRAME        25                      # GPIO pin for SPI frame select
-    @ppINT          22                      # GPIO pin for Interrupt
-    @ppSPI          "spidev0.1"             # SPI channel used
 
-    def init do
-        case :ets.info(:pi_plates) do
-            :undefined ->
-              :ets.new(:pi_plates, [:named_table])
-              :ets.insert(:pi_plates, {:io, init_spi_and_gpio()})
-              :ok
-            _ ->
-              {:error, :already_initialized}
-        end
+  @doc """
+  Initalizes the board
+
+  Sets up the "global I/O context" in ETS so it can be retreived by get_io()
+  Initializes basic features of the board.
+  """
+  @spec init() :: :ok | {:error, :already_initialized}
+
+  # GPIO pin for SPI frame select
+  @ppFRAME 25
+  # GPIO pin for Interrupt
+  @ppINT 22
+  # SPI channel used
+  @ppSPI "spidev0.1"
+
+  @type channel :: integer
+
+  @type addr :: integer
+
+  def init do
+    case :ets.info(:pi_plates) do
+      :undefined ->
+        :ets.new(:pi_plates, [:named_table])
+        :ets.insert(:pi_plates, {:io, init_spi_and_gpio()})
+        :ok
+      _ ->
+        {:error, :already_initialized}
+    end
+  end
+
+  # create a "global I/O" context, which is stored in ETS by init()
+  defp init_spi_and_gpio do
+    {:ok, gpio_frame} = GPIO.open(@ppFRAME, :output)
+    {:ok, gpio_int} = GPIO.open(@ppINT, :input)
+    :ok = GPIO.set_pull_mode(gpio_int, :pullup)
+    {:ok, spi_ref} = SPI.open(@ppSPI)
+
+    %{
+      gpio_frame: gpio_frame,
+      gpio_int: gpio_int,
+      spi: spi_ref
+    }
+  end
+
+  @doc "Return the global I/O references (which are setup by init())"
+  @spec io_context() :: map
+  def io_context do
+    case :ets.lookup(:pi_plates, :io) do
+      [{:io, value}] -> value
+      _ -> raise "DACQ i/o not yet initialized - please call init() first"
+    end
+  end
+
+  defmodule Raw do
+    @moduledoc "Raw I/O functions to talk to DACQ board's microcontroller"
+
+    @gpio_base_addr 8
+    # 1ms delay before each DACQ uController read
+    @dacq_read_delay 1
+
+    @type addr :: DACQ.addr
+
+    @doc "Sends command to the board, without expecting response"
+    @spec cmd(addr, integer, integer, integer) :: :ok
+    def cmd(addr, cmd, param1, param2 \\ 0) when addr <= 7 do
+      io = DACQ.io_context()
+      GPIO.write(io.gpio_frame, 1)
+      {:ok, _} = SPI.transfer(io.spi, <<addr + @gpio_base_addr, cmd, param1, param2>>)
+      GPIO.write(io.gpio_frame, 0)
+      :ok
     end
 
-    # create a "global I/O" context, which is stored in ETS by init()
-    defp init_spi_and_gpio do
-        {:ok, gpio_frame} = GPIO.open(@ppFRAME, :output)
-        {:ok, gpio_int} = GPIO.open(@ppINT, :input)
-        :ok = GPIO.set_pull_mode(gpio_int, :pullup)
-        {:ok, spi_ref} = SPI.open(@ppSPI)
-        %{
-            gpio_frame: gpio_frame,
-            gpio_int: gpio_int,
-            spi: spi_ref
-        }
+    @doc "Sends query command, returns a binary response of <len> bytes"
+    @spec query(addr, integer, integer, integer, integer) :: binary
+    def query(addr, cmd, param1, param2, len) when addr <= 7 and len > 0 do
+      io = DACQ.io_context()
+      GPIO.write(io.gpio_frame, 1)
+      {:ok, _} = SPI.transfer(io.spi, <<addr + @gpio_base_addr, cmd, param1, param2>>)
+      response =
+        0..(len - 1)
+        |> Enum.map(fn _ -> spi_read_single_byte(io.spi) end)
+        |> :erlang.list_to_binary()
+      GPIO.write(io.gpio_frame, 0)
+      response
     end
 
-    @doc "Return the global I/O context (setup by init())"
-    def get_io() do
-        case :ets.lookup(:pi_plates, :io) do
-            [{:io, value}] -> value
-            _ -> raise "I/O not yet initialized - call init()"
-        end
+    # read an SPI byte - see implementation notes in file for timer rationale
+    defp spi_read_single_byte(spi) do
+      :timer.sleep(@dacq_read_delay)
+      {:ok, <<byte>>} = SPI.transfer(spi, <<0x00>>)
+      byte
     end
+  end
+
+  defmodule Board do
+    @moduledoc "Functions relating to enumerating and identifying DACQ boards"
 
     @doc "Return a list of addresses where DACQ boards are present"
-    def list() do
-        Enum.reject(0..7, &(! present?(&1)))
+    @spec address_list() :: list(DACQ.addr)
+    def address_list do
+      Enum.reject(0..7, &(!present?(&1)))
     end
 
-    @doc "Predicate for presense of a board on specified address"
-    @spec present?(integer) :: bool
+    @doc "Returns true if a DACQ board exists on the specified address"
+    @spec present?(integer) :: boolean
     def present?(addr) do
-        proper_response = <<(addr+8)>>
-        case getADDR(addr) do
-            ^proper_response -> true
-            _ -> false
-        end
+      read_raw_address(addr) == (addr + 8)
     end
 
     @doc "Return the string identifier for this board"
-    def board_id(addr) do
-        ppCMD(addr, 0x01, 0, 0, 20)
-        |> String.trim(<<0x00>>)
+    @spec id(DACQ.addr) :: String.t
+    def id(addr) do
+      Raw.query(addr, 0x01, 0, 0, 20)
+      |> String.trim(<<0x00>>)
     end
 
-
-    @doc "Read from the ADC (channel 8 is double scaled for some reason)"
-    def adc_read(addr, channel) when channel <= 7 do
-        adc_read_scaled(addr, channel)
+    @doc "Gets the raw address for a board (should be addr+8)"
+    @spec read_raw_address(DACQ.addr) :: integer
+    def read_raw_address(addr) do
+      <<raw_address>> = Raw.query(addr, 0x00, 0, 0, 1)
+      raw_address
     end
-    def adc_read(addr, channel) when channel == 8 do
-        2 * adc_read_scaled(addr, channel)
-    end
+  end
 
-    defp adc_read_scaled(addr, channel) do
-        <<value::16>> = ppCMD(addr, 0x30, channel, 0, 2)
-        Float.round(value * 4.096/1024, 3)
-    end
+  defmodule ADC do
+    @moduledoc "Functions related to ADC input"
 
-    @doc "Read all ADC as list"
-    # REVIEW - this does not return or scale channel 8
-    def adc_read_all(addr) do
-        bytes = ppCMD(addr, 0x31, 0, 0, 16)
-        <<a::16,b::16,c::16,d::16,e::16,f::16,g::16,h::16>> = bytes
-        [a,b,c,d,e,f,g,h]
-        |> Enum.map(&(Float.round(&1 * 4.096/1024, 3)))
+    @doc "Returns voltage from a single ADC channel(0-7)"
+    @spec read(DACQ.addr, DACQ.channel) :: float
+    def read(addr, channel) when is_integer(channel) and channel <= 7 do
+      read_scaled(addr, channel)
     end
 
-
-    # defp ensure_analog_params(channel, value) do
-    #     cond do
-    #         value < 0 or value > 4.097 ->
-    #             raise ArgumentError, "argument out of range - must be less than 4.097 volts"
-    #         (channel != 0) and (channel != 1) ->
-    #             raise ArgumentError, "channel must be 0 or 1"
-    #         true -> true
-    #     end
-    # end
-
-    # LED Functions
-
-    def led_set(addr, led) do
-        ledCMD(addr, 0x60, led, 0, 0)
+    @doc "Return the system voltage for the DACQ"
+    @spec read_vin(DACQ.addr) :: float
+    def read_vin(addr) do
+      2 * read_scaled(addr, 8)
     end
 
-    def led_clr(addr, led) do
-        ledCMD(addr, 0x61, led, 0, 0)
+    @doc "Returns a list of the voltages from ADC channels 0-7"
+    @spec read_all(DACQ.addr) :: [float]
+    def read_all(addr) do
+      <<a::16, b::16, c::16, d::16, e::16, f::16, g::16, h::16>> =
+        Raw.query(addr, 0x31, 0, 0, 16)
+      [a, b, c, d, e, f, g, h]
+      |> Enum.map(&Float.round(&1 * 4.096 / 1024, 3))
     end
 
-    def led_toggle(addr, led) do
-        ledCMD(addr, 0x62, led, 0, 0)
+    @spec read_scaled(DACQ.addr, DACQ.channel) :: float
+    defp read_scaled(addr, channel) do
+      <<value::16>> = Raw.query(addr, 0x30, channel, 0, 2)
+      Float.round(value * 4.096 / 1024, 3)
+    end
+  end
+
+  defmodule LED do
+    @moduledoc "Manage the bi-color LED on the board (color 0=red, 1=green)"
+
+    @type color :: integer
+    @type addr :: DACQ.addr
+
+    # define macro for guard to ensure valid color (0 or 1)
+    defmacrop is_color(c), do: quote do: (unquote(c) == 0 or unquote(c) == 1)
+
+    @doc "Set (turn on) one of the LED colors"
+    @spec set(addr, color) :: :ok
+    def set(addr, color) when is_color(color) do
+      Raw.cmd(addr, 0x60, color, 0)
     end
 
-    def led_get(addr, led) do
-        ledCMD(addr, 0x63, led, 0, 1)
+    @doc "Clear (turn off) one of the LED colors"
+    @spec clear(addr, color) :: :ok
+    def clear(addr, color) when is_color(color) do
+      Raw.cmd(addr, 0x61, color, 0)
     end
 
-    # LED private helpers
-
-    defp ledCMD(_, _, led, _, _) when led < 0 or led > 1 do
-        {:error, "Invalid LED value"}
-    end
-    defp ledCMD(addr, cmd, led, 0, bytes2return) do
-        ppCMD(addr, cmd, led, 0, bytes2return)
+    @doc "Toggle one of the LED colors"
+    @spec toggle(addr, color) :: :ok
+    def toggle(addr, color) do
+      Raw.cmd(addr, 0x62, color, 0)
     end
 
-    # DOUT (digital output) functions
-
-    def dout_set(addr, bit) do
-        bitCMD(addr, 0x10, bit, 0, 0)
+    @doc "Write the state of an LED color"
+    @spec write(addr, color, boolean) :: :ok
+    def write(addr, color, state) when is_boolean(state) do
+      if state do
+        set(addr, color)
+      else
+        clear(addr, color)
+      end
     end
 
-    def dout_clear(addr, bit) do
-        bitCMD(addr, 0x11, bit, 0, 0)
+    @doc "Read the state of an LED color"
+    @spec read(addr, color) :: boolean
+    def read(addr, color) do
+      <<state>> = Raw.query(addr, 0x63, color, 0, 1)
+      state != 0
+    end
+  end
+
+  defmodule DOUT do
+    @moduledoc "Digital output Functions"
+
+    @type addr :: DACQ.addr
+    @type bit :: integer
+
+    # define guard to ensure bit is valid (integer from 0-6)
+    defmacrop is_bit(b) do
+      quote do: (is_integer(unquote(b)) and unquote(b) >= 0 and unquote(b) <= 6)
     end
 
-    def dout_toggle(addr, bit) do
-        bitCMD(addr, 0x12, bit, 0, 0)
+    @doc "Sets (turns on) a digital output bit"
+    @spec set(addr, bit) :: :ok
+    def set(addr, bit) when is_bit(bit) do
+      Raw.cmd(addr, 0x10, bit)
     end
 
-    def dout_write(addr, bit, value) do
-        cmd = if value do
-            0x10
-        else
-            0x11
-        end
-        bitCMD(addr, cmd, bit, 0, 0)
+    @doc "Clears (turns off) a digital output bit"
+    @spec clear(addr, bit) :: :ok
+    def clear(addr, bit) when is_bit(bit) do
+      Raw.cmd(addr, 0x11, bit)
     end
 
-    def dout_get_all(addr) do
-        ppCMD(addr, 0x14, 0, 0, 1)
+    @doc "Toggles a digital output bit"
+    @spec toggle(addr, bit) :: :ok
+    def toggle(addr, bit) when is_bit(bit) do
+      Raw.cmd(addr, 0x12, bit)
     end
 
-    def dout_set_all(addr, byte) do
-        if byte > 127 do
-            raise ArgumentError, "byte argument out of range - must be less than 128"
-        else
-            ppCMD(addr, 0x13, byte, 0, 0)
-        end
+    @doc "Writes a boolean value to a digital output bit"
+    @spec write(addr, bit, boolean) :: :ok
+    def write(addr, bit, value) when is_bit(bit) and is_boolean(value) do
+      if value do
+        set(addr, bit)
+      else
+        clear(addr, bit)
+      end
     end
 
-    # DOUT private helpers
-
-    defp bitCMD(addr, cmd, bit, arg, bytes2return) do
-        if bit < 0 or bit > 6 do
-            raise ArgumentError, "Invalid BIT specified (range 0-6)"
-        else
-            ppCMD(addr, cmd, bit, arg, bytes2return)
-        end
+    @doc "Reads the state of all bits at once as a byte"
+    @spec read_all(addr) :: integer
+    def read_all(addr) do
+      <<byte>> = Raw.query(addr, 0x14, 0, 0, 1)
+      byte
     end
 
-
-    @doc "Gets the address for a board (but you have to know it)"
-    def getADDR(addr) do
-        ppCMD(addr, 0x00, 0, 0, 1)
+    @doc "Writes a byte to set the state of all bits at once"
+    @spec write_all(addr, integer) :: :ok
+    def write_all(addr, byte) when byte < 128 do
+      Raw.cmd(addr, 0x13, byte)
     end
-
-    @doc "Issues a low level command to the DAQC board"
-
-    @gpio_base_addr 8
-
-    @spec ppCMD(integer, integer, integer, integer, integer) :: :ok | binary
-    def ppCMD(addr, _, _, _, _) when addr > 7 do
-        {:error, "Address parameter must be between 0 and 7"}
-    end
-    def ppCMD(addr, cmd, param1, param2, bytes2return) do
-        io = get_io()
-        GPIO.write(io.gpio_frame, 1)
-        {:ok, _} = SPI.transfer(io.spi, <<addr+@gpio_base_addr, cmd, param1, param2>>)
-        result = if bytes2return > 0 do
-            (0..(bytes2return-1))
-            |> Enum.map(fn (_) -> spi_read_single_byte(io.spi) end)
-            |> :erlang.list_to_binary
-        else
-            :ok
-        end
-        GPIO.write(io.gpio_frame, 0)
-        result
-    end
-
-    defp spi_read_single_byte(spi) do
-        :timer.sleep(@dacq_read_delay)                      # see impl notes
-        {:ok, <<byte>>} = SPI.transfer(spi, <<0x00>>)
-        byte
-    end
+  end
 
 end
+
