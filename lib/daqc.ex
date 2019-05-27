151341dfab8318caa350f2ba53d604e9c9c7e9c5
n6mz <mikemr@cryptolab.net>
2019-05-27 16:56:27 +0000

    changed SPI bus speed to 500kHz (maximum)

--- lib/daqc.ex
+++ lib/daqc.ex
@@ -85,7 +85,7 @@ defmodule DAQC do
   defp init_spi_and_gpio do
     {:ok, gpio_frame} = GPIO.open(@ppFRAME, :output, [{:initial_value, 0}])
     {:ok, gpio_int} = GPIO.open(@ppINT, :input, [{:pull_mode, :pullup}])
-    {:ok, spi_ref} = SPI.open(@ppSPI)
+    {:ok, spi_ref} = SPI.open(@ppSPI, speed_hz: 500000)
 
     %{
       gpio_frame: gpio_frame,
