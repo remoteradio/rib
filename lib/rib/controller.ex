8ceb971119f7775fdcede05cd022510f0463c6c7
n6mz <mikemr@cryptolab.net>
2019-05-28 00:38:23 +0000

    reorganized handlers coreTemp -> core_temp started to add support for multiple boards started to add board address to MQTT handlers

--- lib/rib/controller.ex
+++ lib/rib/controller.ex
@@ -19,11 +19,13 @@ defmodule Rib.Controller do
   def init(_args) do
     :ok = DAQC.init()
     Tortoise.publish RIB, "rib/controller/time_last_started", timestamp(), retain: true
-    Tortoise.publish RIB, "rib/daqc/id", DAQC.Board.id(0), retain: true
     Tortoise.publish RIB, "rib/daqc/address_list", inspect(DAQC.Board.address_list()), retain: true
+    for address <- 0..1 do
+      Tortoise.publish RIB, "rib/daqc/#{address}/id", DAQC.Board.id(address), retain: true
+    end
 
     # send the first messages to kick off 100ms, 1000ms, and 5000ms repetitve ticks
-    send self(), :tick_100        # must be sent first to force read and init state.daqc hash
+    send self(), :tick_500        # must be sent first to force read and init state.daqc hash
     send self(), :tick_1000
     send self(), :tick_5000
 
@@ -38,44 +40,48 @@ defmodule Rib.Controller do
     handle_mqtt(subtopic, payload, state)
   end
 
-  # invoked every 5000ms when we receive the :tick_5000 message
-  def handle_info(:tick_5000, state) do
-    # get the temperature of the SoC core and format as a rounded float and then publish
-    # only do this if the /sys/class/thermal filesystem exists (not on macOS)
-    case File.read "/sys/class/thermal/thermal_zone0/temp" do
-      {:ok, coreTemp} ->
-        {coreTemp, _} = Integer.parse(coreTemp)
-        coreTemp = Kernel.round(coreTemp / 1000.0)
-        coreTemp = Integer.to_string(coreTemp)
-        Tortoise.publish RIB, "rib/controller/SoC_core_temp", coreTemp, retain: true
-      _ -> nil
-    end
-    Process.send_after self(), :tick_5000, 5000    # schedule another tick in another 5000ms
-    {:noreply, state}
-  end
-
-  # invoked every 1000ms when we receive the :tick_1000 message
-  def handle_info(:tick_1000, state) do
-    Tortoise.publish RIB, "rib/controller/time_last_updated", timestamp(), retain: true
-    Tortoise.publish RIB, "rib/daqc/led/color", Atom.to_string(DAQC.LED.get_color(0))
-    publish_dac_values(0, state)
-    publish_dac_values(1, state)
-    Process.send_after self(), :tick_1000, 1000    # schedule another tick in another 1000ms
-    {:noreply, state}
-  end
-
-  # invoked every 100ms when we receive the :tick_100 message
+  # invoked every 500ms when we receive the :tick_500 message
   # walk through all topics in the daqc_topics map, and only publish changes to the ones
   # REVIEW replace inspect(..) below with a better way of converting to string payload
-  def handle_info(:tick_100, state) do
+  def handle_info(:tick_500, state) do
+    Tortoise.publish RIB, "rib/controller/time_last_updated", timestamp(), retain: true
     new_daqc = read_daqc(0)
     Enum.each @daqc_topics, fn {key, subtopic} ->
       if (state.daqc[key] != new_daqc[key]) do
         Tortoise.publish RIB, "rib/daqc/#{subtopic}", inspect(new_daqc[key])
       end
     end
-    Process.send_after self(), :tick_100, 100    # schedule another tick in another 100ms
+    Process.send_after self(), :tick_500, 500    # schedule another tick in another 100ms
     {:noreply, %{state | daqc: new_daqc}}
+  end
+
+  # invoked every 1000ms when we receive the :tick_1000 message
+  def handle_info(:tick_1000, state) do
+    for address <- 0..1 do
+      Tortoise.publish RIB, "rib/daqc/#{address}/led/color", Atom.to_string(DAQC.LED.get_color(address))
+      for channel <- 0..1 do
+        publish_dac_values(address, channel, state)
+        publish_dac_values(address, channel, state)
+      end
+    end
+    Process.send_after self(), :tick_1000, 1000    # schedule another tick in another 1000ms
+    {:noreply, state}
+  end
+
+  # invoked every 5000ms when we receive the :tick_5000 message
+  def handle_info(:tick_5000, state) do
+    # get the temperature of the SoC core and format as a rounded float and then publish
+    # only do this if the /sys/class/thermal filesystem exists (not on macOS)
+    case File.read "/sys/class/thermal/thermal_zone0/temp" do
+      {:ok, core_temp} ->
+        {core_temp, _} = Integer.parse(core_temp)
+        core_temp = Kernel.round(core_temp / 1000.0)
+        core_temp = Integer.to_string(core_temp)
+        Tortoise.publish RIB, "rib/controller/SoC_core_temp", core_temp, retain: true
+      _ -> nil
+    end
+    Process.send_after self(), :tick_5000, 5000    # schedule another tick in another 5000ms
+    {:noreply, state}
   end
 
   # MQTT HANDLERS
@@ -85,32 +91,32 @@ defmodule Rib.Controller do
   #
   # For now, these assume board 0.
 
-  def handle_mqtt(["daqc", "led", "color", "set"], payload, state) do
-    DAQC.LED.set_color(0, String.to_atom(payload))
+  def handle_mqtt(["daqc", address, "led", "color", "set"], payload, state) do
+    DAQC.LED.set_color(String.to_integer(address), String.to_atom(payload))
     {:noreply, state}
   end
-  def handle_mqtt(["daqc", "dac", channel, "raw", "set"], payload, state) do
-    DAQC.DAC.write(0, String.to_integer(channel), String.to_integer(payload))
+  def handle_mqtt(["daqc", address, "dac", channel, "raw", "set"], payload, state) do
+    DAQC.DAC.write(String.to_integer(address), String.to_integer(channel), String.to_integer(payload))
     {:noreply, state}
   end
-  def handle_mqtt(["daqc", "dac", channel, "set"], payload, state) do
+  def handle_mqtt(["daqc", address, "dac", channel, "set"], payload, state) do
     case Float.parse(payload) do
       {volts, _rest} ->
         dac_value = (volts / state.daqc[:adc_vin]) * 1024
-        DAQC.DAC.write(0, String.to_integer(channel), floor(dac_value))
+        DAQC.DAC.write(String.to_integer(address), String.to_integer(channel), floor(dac_value))
       :error ->
         Logger.warn "Invalid DAC voltage requested: #{payload}"
     end
     {:noreply, state}
   end
-  def handle_mqtt(["daqc", "dout", "all", "set"], payload, state) do
+  def handle_mqtt(["daqc", address, "dout", "all", "set"], payload, state) do
     Logger.info "Got daqc/dout/all/set with payload #{payload}"
-    DAQC.DOUT.write_all(0, String.to_integer(payload))
+    DAQC.DOUT.write_all(String.to_integer(address), String.to_integer(payload))
     {:noreply, state}
   end
-  def handle_mqtt(["daqc", "dout", bit, "set"], payload, state) do
+  def handle_mqtt(["daqc", address, "dout", bit, "set"], payload, state) do
     Logger.info "Got daqc/dout/#{bit}/set with payload #{payload}"
-    DAQC.DOUT.write(0, String.to_integer(bit), String.to_integer(payload))
+    DAQC.DOUT.write(String.to_integer(address), String.to_integer(bit), String.to_integer(payload))
     {:noreply, state}
   end
   def handle_mqtt(["test", "logme"], payload, state) do
@@ -127,11 +133,11 @@ defmodule Rib.Controller do
   # both this raw value (at daqc/dac/<channel>/raw) and the computed voltage
   # (at daqc/dac/<channel>).   Needs to be passed the state so we can calibarate
   # the DAC values against the current Vcc (which is in state.daqc[:adc_vin])
-  defp publish_dac_values(channel, state) do
-    raw_value = DAQC.DAC.read(0, channel)
+  defp publish_dac_values(address, channel, state) do
+    raw_value = DAQC.DAC.read(address, channel)
     volts = (raw_value * state.daqc[:adc_vin]) / 1024
-    Tortoise.publish RIB, "rib/daqc/dac/#{channel}/raw", Integer.to_string(raw_value)
-    Tortoise.publish RIB, "rib/daqc/dac/#{channel}", Float.to_string(volts)
+    Tortoise.publish RIB, "rib/daqc/#{address}/dac/#{channel}/raw", Integer.to_string(raw_value)
+    Tortoise.publish RIB, "rib/daqc/#{address}/dac/#{channel}", Float.to_string(volts)
   end
 
   # Return a map with values that reflect the current DAQC state for each key
